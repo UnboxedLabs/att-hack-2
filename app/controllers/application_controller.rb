67cbd3aad6862db79da79bfe4a2efd6aa79e4cd8
Leo Correa <lcorr005@gmail.com>
2012-12-15 17:08:02 -0500

    Added debugs to global

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_filter :set_default_response_format
   before_filter :restrict_access
+  before_filter :params_incoming
 
+  def params_incoming
+    puts "Params Incoming!"
+    puts params
+  end
 private
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
