29e02df50bc9c499c9ab0cd10b663c3a2fbd3e52
Leo Correa <lcorr005@gmail.com>
2012-12-14 22:44:52 -0500

    Added api security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   before_filter :set_default_response_format
+  before_filter :restrict_access
 
 private
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+    end
+  end
   def set_default_response_format
     request.format = :json unless params[:format]
   end
