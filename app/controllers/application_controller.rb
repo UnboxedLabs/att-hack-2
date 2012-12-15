c79939dac6b65165d9e3b314b95a032b67003418
Leo Correa <lcorr005@gmail.com>
2012-12-14 21:43:34 -0500

    Request format json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  before_filter :set_default_response_format
+
+private
+  def set_default_response_format
+    request.format = :json unless params[:format]
+  end
 end
