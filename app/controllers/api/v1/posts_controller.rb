9aa8b86d3c50f916d46044df85a267491b265f1a
Leo Correa <lcorr005@gmail.com>
2012-12-15 10:52:57 -0500

    Posts controller

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      def create
+      end
+
+      def index
+      end
+
+      def show
+      end
+    end
+  end
+end
