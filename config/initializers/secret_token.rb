ac56271bff06260647e634df6914dd72a582ddcd
Leo Correa <lcorr005@gmail.com>
2012-12-14 21:26:45 -0500

    The most awesome commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AttHack2::Application.config.secret_token = '02a789b0feab45a2431ca51bc0127157943449906fbd921f77d0620f9125de8fb5b63f2cef62cde3507383be22c53df736fad931f97ca2bcdfdd63a1e6572d73'
